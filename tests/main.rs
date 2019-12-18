1b26f8477c42da10786cb510c2fd42cd512e4a6d
Zachary Newman <z@znewman.net>
2019-12-18 15:15:46 -0500

    chore: Fix clippy lints

--- tests/main.rs
+++ tests/main.rs
@@ -3,6 +3,7 @@ extern crate spectrum_impl;
 #[test]
 fn test_pass() {
     // TODO: tokio::run in tokio 0.2.0+
+    #![allow(unused_must_use)]
     tokio::runtime::Runtime::new()
         .unwrap()
         .block_on(spectrum_impl::run());
