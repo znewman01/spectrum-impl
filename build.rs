13577925ddbb1273c1de11a05c6544b4951c7d82
Zachary Newman <z@znewman.net>
2020-01-06 16:27:48 -0500

    Remove dummy protos

--- build.rs
+++ build.rs
@@ -1,5 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/prototest.proto")?;
     tonic_build::compile_protos("proto/spectrum.proto")?;
     Ok(())
 }
