b3ff4e53d33eed1057848394c49ba8235213895f
Zachary Newman <z@znewman.net>
2020-01-07 13:41:01 -0500

    Add a logger
    So now we can see output!

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,23 @@
+use simplelog::{CombinedLogger, LevelFilter, TermLogger, TerminalMode, WriteLogger};
+use std::fs::File;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    CombinedLogger::init(vec![
+        TermLogger::new(
+            LevelFilter::Trace,
+            simplelog::ConfigBuilder::new()
+                .add_filter_allow_str("spectrum_impl")
+                .build(),
+            TerminalMode::Stderr,
+        )
+        .unwrap(),
+        WriteLogger::new(
+            LevelFilter::Trace,
+            simplelog::Config::default(),
+            File::create("spectrum.log").unwrap(),
+        ),
+    ])
+    .unwrap();
     spectrum_impl::run().await
 }
--- tests/main.rs
+++ tests/main.rs
@@ -1,9 +1,19 @@
 extern crate spectrum_impl;
 
+use simplelog::{LevelFilter, TermLogger, TerminalMode};
+
 #[test]
 fn test_pass() {
     // TODO: tokio::run in tokio 0.2.0+
     #![allow(unused_must_use)]
+    TermLogger::init(
+        LevelFilter::Trace,
+        simplelog::ConfigBuilder::new()
+            .add_filter_allow_str("spectrum_impl")
+            .build(),
+        TerminalMode::Stderr,
+    )
+    .unwrap();
     tokio::runtime::Runtime::new()
         .unwrap()
         .block_on(spectrum_impl::run());
