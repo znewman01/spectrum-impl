d82f84c5e03a0f6264f16f4823aff2a517442c78
Zachary Newman <z@znewman.net>
2020-02-02 13:40:18 -0500

    Implement (no-op) publisher

--- src/bin/publisher.rs
+++ src/bin/publisher.rs
@@ -1,9 +1,11 @@
+use futures::prelude::*;
 use spectrum_impl::{config, publisher, services::PublisherInfo};
+use tokio::signal::ctrl_c;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Sync + Send>> {
     let config_store = config::from_env()?;
     // TODO(zjn): construct from environment/args
     let info = PublisherInfo::new();
-    publisher::run(config_store, info).await
+    publisher::run(config_store, info, ctrl_c().map(|_| ())).await
 }
