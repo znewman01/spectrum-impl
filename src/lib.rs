fe7fcaf911e3adc4e3a218fc9b9c308b1dfa055b
Zachary Newman <z@znewman.net>
2020-02-02 13:40:18 -0500

    Implement (no-op) publisher

--- src/lib.rs
+++ src/lib.rs
@@ -18,9 +18,9 @@ use experiment::Experiment;
 use services::Service::{Leader, Publisher, Worker};
 
 pub async fn run() -> Result<(), Box<dyn std::error::Error + Sync + Send>> {
-    let config_store = config::from_env()?;
+    let config = config::from_env()?;
     let experiment = Experiment::new(2, 2, 5);
-    experiment::write_to_store(&config_store, experiment).await?;
+    experiment::write_to_store(&config, experiment).await?;
     let barrier = Arc::new(Barrier::new(
         experiment.clients as usize + experiment.iter_services().count(),
     ));
@@ -32,29 +32,23 @@ pub async fn run() -> Result<(), Box<dyn std::error::Error + Sync + Send>> {
             barrier.wait().await;
             Ok(())
         };
-        handles.push(
-            client::run(config_store.clone())
-                .and_then(|_| shutdown)
-                .boxed(),
-        );
+        handles.push(client::run(config.clone()).and_then(|_| shutdown).boxed());
     }
 
     for service in experiment.iter_services() {
         let barrier = barrier.clone();
         let shutdown = async move {
             barrier.wait().await;
-            Ok(())
         };
 
         handles.push(match service {
-            Publisher(info) => publisher::run(config_store.clone(), info)
-                .and_then(|_| shutdown)
-                .boxed(),
-            Leader(info) => leader::run(config_store.clone(), info, shutdown.map(|_| ())).boxed(),
-            Worker(info) => worker::run(config_store.clone(), info, shutdown.map(|_| ())).boxed(),
+            Publisher(info) => publisher::run(config.clone(), info, shutdown).boxed(),
+            Leader(info) => leader::run(config.clone(), info, shutdown).boxed(),
+            Worker(info) => worker::run(config.clone(), info, shutdown).boxed(),
         });
     }
 
+    // TODO(zjn): log better
     handles.try_for_each(|_| futures::future::ok(())).await?;
 
     Ok(())
--- src/publisher.rs
+++ src/publisher.rs
@@ -4,19 +4,63 @@ use crate::{
     net::get_addr,
     services::{
         discovery::{register, Node},
+        health::{wait_for_health, AllGoodHealthServer, HealthServer},
         quorum::{set_start_time, wait_for_quorum},
         PublisherInfo,
     },
 };
-use chrono::prelude::*;
-use log::{debug, info};
 
-pub async fn run<C: Store>(
+use chrono::prelude::*;
+use futures::Future;
+use log::{debug, error, info, trace};
+use tonic::{Request, Response, Status};
+
+pub mod spectrum {
+    tonic::include_proto!("spectrum");
+}
+
+use spectrum::{
+    publisher_server::{Publisher, PublisherServer},
+    AggregateGroupRequest, AggregateGroupResponse,
+};
+
+#[derive(Default)]
+pub struct MyPublisher {}
+
+#[tonic::async_trait]
+impl Publisher for MyPublisher {
+    async fn aggregate_group(
+        &self,
+        _request: Request<AggregateGroupRequest>,
+    ) -> Result<Response<AggregateGroupResponse>, Status> {
+        error!("Not implemented.");
+        Err(Status::new(
+            tonic::Code::Unimplemented,
+            "Not implemented".to_string(),
+        ))
+    }
+}
+
+pub async fn run<C, F>(
     config: C,
     info: PublisherInfo,
-) -> Result<(), Box<dyn std::error::Error + Sync + Send>> {
+    shutdown: F,
+) -> Result<(), Box<dyn std::error::Error + Sync + Send>>
+where
+    C: Store,
+    F: Future<Output = ()> + Send + 'static,
+{
     info!("Publisher starting up.");
     let addr = get_addr();
+    let server_task = tokio::spawn(
+        tonic::transport::server::Server::builder()
+            .add_service(HealthServer::new(AllGoodHealthServer::default()))
+            .add_service(PublisherServer::new(MyPublisher::default()))
+            .serve_with_shutdown(addr, shutdown),
+    );
+
+    wait_for_health(format!("http://{}", addr)).await?;
+    trace!("Publisher {:?} healthy and serving.", info);
 
     let node = Node::new(info.into(), addr);
     register(&config, node).await?;
@@ -29,6 +73,7 @@ pub async fn run<C: Store>(
     info!("Registering experiment start time: {}", dt);
     set_start_time(&config, dt).await?;
 
+    server_task.await??;
     info!("Publisher shutting down.");
 
     Ok(())
