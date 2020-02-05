9fb874465e28b0b12d674994d299ee2a80a78646
Sacha Servan-Schreiber <sachaservanschreiber@gmail.com>
2020-02-05 11:29:51 -0500

    update main protocol functions to use lss instead of adhoc sharing

--- src/crypto/protocols.rs
+++ src/crypto/protocols.rs
@@ -2,10 +2,9 @@
 #![allow(dead_code)]
 
 extern crate rand;
-use crate::crypto::byte_utils::{xor_bytes, xor_bytes_list};
-use crate::crypto::dpf::{DPFKey, PRGBasedDPF, DPF};
+use crate::crypto::dpf::{DPFKey, PRGBasedDPF};
 use crate::crypto::field::{Field, FieldElement};
-use bytes::Bytes;
+use crate::crypto::lss::{SecretShare, LSS};
 use rug::{rand::RandState, Integer};
 use std::rc::Rc;
 
@@ -20,14 +19,15 @@ struct CryptoParams {
 // TODO: make sure it matches with protobufs
 #[derive(Clone, PartialEq, Debug)]
 struct ServerAuditToken {
-    bit_check_token: FieldElement,
-    seed_check_token: FieldElement,
+    bit_check_token: SecretShare,
+    seed_check_token: SecretShare,
+    msg_check_token: FieldElement,
 }
 
 #[derive(Clone, PartialEq, Debug)]
 struct ClientProofShare {
-    bit_proof_share: FieldElement,
-    seed_proof_share: FieldElement,
+    bit_proof_share: SecretShare,
+    seed_proof_share: SecretShare,
 }
 
 impl CryptoParams {
@@ -55,14 +55,14 @@ impl CryptoParams {
     ) -> ServerAuditToken {
         assert_eq!(channel_keys.len(), dpf_key.bits.len());
         assert_eq!(channel_keys.len(), dpf_key.seeds.len());
-        
+
         let mut res_seed = FieldElement::zero(self.field.clone());
         let mut res_bit = FieldElement::zero(self.field.clone());
 
         for (i, (seed, bit)) in dpf_key.seeds.iter().zip(dpf_key.bits.iter()).enumerate() {
             assert!(*bit == 0 || *bit == 1);
 
-            res_seed += channel_keys[i].clone()
+            res_seed -= channel_keys[i].clone()
                 * FieldElement::from_bytes(seed.raw_bytes(), self.field.clone());
 
             if *bit == 1 {
@@ -70,25 +70,35 @@ impl CryptoParams {
             }
         }
 
-        // TODO(sss): check the hash of the encoded message
+        let mut bit_check_token = client_proof.bit_proof_share.clone();
+        let mut seed_check_token = client_proof.seed_proof_share;
+        bit_check_token.scalar_add(res_bit);
+        seed_check_token.scalar_add(res_seed);
 
+        // TODO(sss): actually hash the message?
+        let msg_check_token = FieldElement::from_bytes(dpf_key.encoded_msg, self.field.clone());
 
         // evaluate the compressed DPF for the given dpf_key
         ServerAuditToken {
-            bit_check_token:  client_proof.bit_proof_share + res_bit,
-            seed_check_token: client_proof.seed_proof_share - res_seed,
+            bit_check_token,
+            seed_check_token,
+            msg_check_token,
         }
     }
 
     /// checks that the set of audit tokens sums to zero in which case
     /// the audit succeeds
     pub fn check_audit(&self, token_a: ServerAuditToken, token_b: ServerAuditToken) -> bool {
-        let sum_bits = token_a.bit_check_token - token_b.bit_check_token;
-        let sum_seeds = token_a.seed_check_token - token_b.seed_check_token;
+        // let sum_bits = token_a.bit_check_token - token_b.bit_check_token;
+        // let sum_seeds = token_a.seed_check_token - token_b.seed_check_token;
+
+        let bit_check = LSS::recover(vec![token_a.bit_check_token, token_b.bit_check_token]);
+        let seed_check = LSS::recover(vec![token_a.seed_check_token, token_b.seed_check_token]);
 
         // check if sum is all-zero
-        sum_seeds == FieldElement::zero(self.field.clone())
-            && sum_bits == FieldElement::zero(self.field.clone())
+        bit_check == FieldElement::zero(self.field.clone())
+            && seed_check == FieldElement::zero(self.field.clone())
+            && (token_a.msg_check_token == token_b.msg_check_token)
     }
 
     /// generates secret shares for the proof attesting to the correctness of the
@@ -107,9 +117,16 @@ impl CryptoParams {
         let mut res_seed_a = FieldElement::zero(self.field.clone());
         let mut res_seed_b = FieldElement::zero(self.field.clone());
 
+        /* 1) generate the proof using the DPF keys and the channel key */
+
         let mut proof_correction = 1;
-        
-        for (i, (seed, bit)) in dpf_key_a.seeds.iter().zip(dpf_key_a.bits.iter()).enumerate() {
+
+        for (i, (seed, bit)) in dpf_key_a
+            .seeds
+            .iter()
+            .zip(dpf_key_a.bits.iter())
+            .enumerate()
+        {
             assert!(*bit == 0 || *bit == 1);
             res_seed_a += FieldElement::from_bytes(seed.raw_bytes(), self.field.clone());
 
@@ -122,42 +139,36 @@ impl CryptoParams {
             assert!(*bit == 0 || *bit == 1);
             res_seed_b += FieldElement::from_bytes(seed.raw_bytes(), self.field.clone());
         }
-        
-        let proof_bits = channel_key.clone() * FieldElement::new(Integer::from(proof_correction), self.field.clone()); 
-        let proof_seeds = channel_key * (res_seed_a.clone() - res_seed_b.clone());
 
-        // TODO(sss): move this to a Share function or something.
-        let mut shares: Vec<ClientProofShare> = Vec::new();
+        /* 2) split the proof into secret shares */
 
-        // sum of the random field elements
-        let mut rand_sum_bits = FieldElement::zero(self.field.clone());
-        let mut rand_sum_seeds = FieldElement::zero(self.field.clone());
+        let bit_proof = channel_key.clone()
+            * FieldElement::new(Integer::from(proof_correction), self.field.clone());
+        let seed_proof = channel_key * (res_seed_a - res_seed_b);
+
         let mut rng = RandState::new();
+        let bit_proof_shares = LSS::share(bit_proof, dpf_keys.len(), &mut rng);
+        let seed_proof_shares = LSS::share(seed_proof, dpf_keys.len(), &mut rng);
+        let mut proof_shares: Vec<ClientProofShare> = Vec::new();
 
-        for _ in 0..self.num_servers - 1 {
-            shares.push(ClientProofShare {
-                bit_proof_share: FieldElement::rand_element(&mut rng, self.field.clone()),
-                seed_proof_share: FieldElement::rand_element(&mut rng, self.field.clone()),
+        for (bit_proof_share, seed_proof_share) in
+            bit_proof_shares.iter().zip(seed_proof_shares.iter())
+        {
+            proof_shares.push(ClientProofShare {
+                bit_proof_share: (*bit_proof_share).clone(),
+                seed_proof_share: (*seed_proof_share).clone(),
             });
-
-            // add the random shares to the sum total
-            rand_sum_bits += shares.last().unwrap().bit_proof_share.clone();
-            rand_sum_seeds += shares.last().unwrap().seed_proof_share.clone();
         }
 
-        // last share set to proof + SUM rand
-        shares.insert(0, ClientProofShare {
-            bit_proof_share: proof_bits + rand_sum_bits,
-            seed_proof_share: proof_seeds + rand_sum_seeds,
-        });
-
-        shares
+        proof_shares
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use bytes::Bytes;
+    use crate::crypto::dpf::DPF;
 
     fn random_field() -> Rc<Field> {
         let mut p = Integer::from(800_000_000);
@@ -176,16 +187,23 @@ mod tests {
 
         let channel_keys = vec![FieldElement::rand_element(&mut rng, field.clone()); num_chan];
         let dpf = PRGBasedDPF::new(sec_bytes, num_servers, num_chan);
-        let dpf_keys = dpf.gen(Bytes::from(vec![0; sec_bytes * 2]), chan_idx);
+        let dpf_keys = dpf.gen(Bytes::from(vec![0; sec_bytes * 10]), chan_idx);
         let params = CryptoParams::new(num_chan, num_servers, dpf, field);
 
         let client_proof_shares =
             params.gen_proof_shares(chan_idx, channel_keys[chan_idx].clone(), dpf_keys.clone());
 
-        let token_a = params.gen_audit_token(channel_keys.clone(), dpf_keys[0].clone(), client_proof_shares[0].clone());
-        let token_b = params.gen_audit_token(channel_keys.clone(), dpf_keys[1].clone(), client_proof_shares[1].clone());
+        let token_a = params.gen_audit_token(
+            channel_keys.clone(),
+            dpf_keys[0].clone(),
+            client_proof_shares[0].clone(),
+        );
+        let token_b = params.gen_audit_token(
+            channel_keys,
+            dpf_keys[1].clone(),
+            client_proof_shares[1].clone(),
+        );
 
-        
         assert_eq!(params.check_audit(token_a, token_b), true);
     }
 }
