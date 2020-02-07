0ee2e06d3f3482a1f46fe403684834513e3f0537
Sacha Servan-Schreiber <sachaservanschreiber@gmail.com>
2020-02-07 09:22:00 -0500

    simplify and add tests

--- src/crypto/lss.rs
+++ src/crypto/lss.rs
@@ -1,6 +1,6 @@
 //! Spectrum implementation.
 use crate::crypto::field::{Field, FieldElement};
-use rug::{rand::RandState, Integer};
+use rug::rand::RandState;
 use std::fmt::Debug;
 use std::ops;
 use std::rc::Rc;
@@ -20,24 +20,14 @@ impl SecretShare {
     fn new(value: FieldElement) -> SecretShare {
         SecretShare { value }
     }
-
-    pub fn scalar_add(&mut self, scalar: FieldElement) {
-        self.value = self.value.clone() + scalar
-    }
-
-    pub fn scalar_mul(&mut self, scalar: FieldElement) {
-        self.value = self.value.clone() * scalar
-    }
 }
 
 impl LSS {
     pub fn share(value: FieldElement, n: usize, rng: &mut RandState) -> Vec<SecretShare> {
-        if n < 2 {
-            panic!("cannot split secret into fewer than two shares!");
-        }
+        assert!(n >= 2, "cannot split secret into fewer than two shares!");
 
         let mut shares = Vec::<SecretShare>::new();
-        let mut rand_sum = FieldElement::new(Integer::from(0), value.clone().field());
+        let mut rand_sum = FieldElement::new(0.into(), value.clone().field());
 
         let field: Rc<Field> = value.clone().field();
 
@@ -56,9 +46,10 @@ impl LSS {
     }
 
     pub fn recover(shares: Vec<SecretShare>) -> FieldElement {
-        if shares.len() < 2 {
-            panic!("need at least two shares to recover a secret!");
-        }
+        assert!(
+            shares.len() >= 2,
+            "need at least two shares to recover a secret!"
+        );
 
         // recover the secret by subtracting the random shares (mask)
         let mut secret = shares[0].value.clone();
@@ -94,7 +85,14 @@ impl ops::Sub<SecretShare> for SecretShare {
     }
 }
 
-/// override * operation: want multiplication by constant field element
+impl ops::Add<FieldElement> for SecretShare {
+    type Output = SecretShare;
+
+    fn add(self, constant: FieldElement) -> SecretShare {
+        SecretShare::new(self.value + constant)
+    }
+}
+
 impl ops::Mul<FieldElement> for SecretShare {
     type Output = SecretShare;
 
@@ -109,13 +107,11 @@ mod tests {
     use crate::crypto::field::Field;
     use std::rc::Rc;
 
-    // TODO(sss): Add tests for add/sub/mul, etc.
-
     #[test]
     fn test_share_recover() {
         let mut rng = RandState::new();
-        let field = Field::new(Integer::from(101)); // 101 is prime
-        let value = FieldElement::new(Integer::from(100), Rc::<Field>::new(field));
+        let field = Field::new(101.into()); // 101 is prime
+        let value = FieldElement::new(100.into(), Rc::<Field>::new(field));
 
         assert_eq!(value.clone(), LSS::recover(LSS::share(value, 10, &mut rng)));
     }
@@ -123,8 +119,8 @@ mod tests {
     #[test]
     fn test_share_splitting() {
         let mut rng = RandState::new();
-        let field = Field::new(Integer::from(101)); // 101 is prime
-        let value = FieldElement::new(Integer::from(100), Rc::<Field>::new(field));
+        let field = Field::new(101.into()); // 101 is prime
+        let value = FieldElement::new(100.into(), Rc::<Field>::new(field));
 
         // Share generates different shares each time
         assert_ne!(
@@ -134,35 +130,90 @@ mod tests {
     }
 
     #[test]
-    fn test_share_scalar_add() {
+    fn test_share_add() {
         let mut rng = RandState::new();
-        let field = Rc::<Field>::new(Field::new(Integer::from(101))); // 101 is prime
-        let value = FieldElement::new(Integer::from(100), field.clone());
-        let mut shares = LSS::share(value.clone(), 10, &mut rng);
-        let scalar = FieldElement::new(Integer::from(5), field);
-        shares[0].scalar_add(scalar.clone());
-        assert_eq!(value + scalar, LSS::recover(shares));
+        let field = Rc::<Field>::new(Field::new(101.into())); // 101 is prime
+
+        // setup
+        let value1 = FieldElement::new(100.into(), field.clone());
+        let value2 = FieldElement::new(100.into(), field);
+        let shares1 = LSS::share(value1.clone(), 10, &mut rng);
+        let shares2 = LSS::share(value2.clone(), 10, &mut rng);
+
+        // test share addition
+        let mut shares_sum: Vec<SecretShare> = Vec::new();
+        for (share1, share2) in shares1.iter().zip(shares2.iter()) {
+            shares_sum.push(share1.clone() + share2.clone());
+        }
+
+        // make sure adding shares results in the correct sum
+        assert_eq!(value1 + value2, LSS::recover(shares_sum));
     }
 
     #[test]
-    fn test_share_scalar_mul() {
+    fn test_share_sub() {
         let mut rng = RandState::new();
-        let field = Rc::<Field>::new(Field::new(Integer::from(101))); // 101 is prime
-        let value = FieldElement::new(Integer::from(100), field.clone());
-        let mut shares = LSS::share(value.clone(), 10, &mut rng);
-        let scalar = FieldElement::new(Integer::from(5), field);
-        for share in shares.iter_mut() {
-            share.scalar_mul(scalar.clone());
+        let field = Rc::<Field>::new(Field::new(101.into())); // 101 is prime
+
+        // setup
+        let value1 = FieldElement::new(100.into(), field.clone());
+        let value2 = FieldElement::new(100.into(), field);
+        let shares1 = LSS::share(value1.clone(), 10, &mut rng);
+        let shares2 = LSS::share(value2.clone(), 10, &mut rng);
+
+        // test share subtraction
+        let mut shares_sum: Vec<SecretShare> = Vec::new();
+        for (share1, share2) in shares1.iter().zip(shares2.iter()) {
+            shares_sum.push(share1.clone() - share2.clone());
         }
-        assert_eq!(value * scalar, LSS::recover(shares));
+
+        // assert that subtraction works
+        assert_eq!(value1 - value2, LSS::recover(shares_sum));
+    }
+
+    #[test]
+    fn test_share_constant_add() {
+        let mut rng = RandState::new();
+        let field = Rc::<Field>::new(Field::new(101.into())); // 101 is prime
+        let value = FieldElement::new(100.into(), field.clone());
+
+        // share the value
+        let mut shares = LSS::share(value.clone(), 10, &mut rng);
+
+        // add a constant to it
+        let constant = FieldElement::new(5.into(), field);
+        shares[0] = shares[0].clone() + constant.clone();
+
+        // make sure the recovered shares are correct
+        assert_eq!(value + constant, LSS::recover(shares));
+    }
+
+    #[test]
+    fn test_share_constant_mul() {
+        let mut rng = RandState::new();
+        let field = Rc::<Field>::new(Field::new(101.into())); // 101 is prime
+        let value = FieldElement::new(100.into(), field.clone());
+
+        // share the value
+        let mut shares = LSS::share(value.clone(), 10, &mut rng);
+
+        // multiply all shares by a constant
+        let constant = FieldElement::new(5.into(), field);
+        for share in shares.iter_mut() {
+            *share = (*share).clone() * constant.clone();
+        }
+
+        // ensure correct recovery
+        assert_eq!(value * constant, LSS::recover(shares));
     }
 
     #[test]
     fn test_share_different_n() {
         let mut rng = RandState::new();
-        let field = Field::new(Integer::from(101)); // 101 is prime
-        let value = FieldElement::new(Integer::from(100), Rc::<Field>::new(field));
+        let field = Field::new(101.into()); // 101 is prime
+        let value = FieldElement::new(100.into(), Rc::<Field>::new(field));
 
+        // share with 2-5 way splits and make sure the shares are recoverable
         let rec2 = LSS::recover(LSS::share(value.clone(), 2, &mut rng));
         let rec3 = LSS::recover(LSS::share(value.clone(), 3, &mut rng));
         let rec4 = LSS::recover(LSS::share(value.clone(), 4, &mut rng));
@@ -178,8 +229,8 @@ mod tests {
     #[should_panic]
     fn test_share_invalid() {
         let mut rng = RandState::new();
-        let field = Field::new(Integer::from(101)); // 101 is prime
-        let value = FieldElement::new(Integer::from(100), Rc::<Field>::new(field));
+        let field = Field::new(101.into()); // 101 is prime
+        let value = FieldElement::new(100.into(), Rc::<Field>::new(field));
         LSS::share(value, 1, &mut rng);
     }
 }
