499188d927f5d2b9b066e047b7592058c5d95206
Sacha Servan-Schreiber <sachaservanschreiber@gmail.com>
2020-02-10 07:57:52 -0500

    use openssl aes for 10x performance boost
    rm prg seed size

--- benches/crypto_benchmarks.rs
+++ benches/crypto_benchmarks.rs
@@ -5,15 +5,14 @@ use criterion::Criterion;
 use spectrum_impl::crypto::prg::PRG;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let seed_size: usize = 16;
     let eval_size: usize = 1 << 20; // approx 1M bytes
-    let prg = PRG::new(seed_size, eval_size);
+    let prg = PRG::new();
     let seed = prg.new_seed();
 
     c.bench_function("PRG eval benchmark", |b| {
         b.iter(|| {
             // benchmark the PRG evaluation time
-            prg.eval(&seed);
+            prg.eval(&seed, eval_size);
         })
     });
 }
