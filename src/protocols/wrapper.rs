9dbd4570bdbb98fc7f42829e9f747310aa9676eb
Zachary Newman <z@znewman.net>
2020-03-17 14:27:28 -0400

    Add wrapper for new_accumulator()

--- src/protocols/wrapper.rs
+++ src/protocols/wrapper.rs
@@ -1,11 +1,24 @@
 use crate::proto::{AuditShare, WriteToken};
-use crate::protocols::{ChannelKeyWrapper, Protocol};
+use crate::protocols::{Accumulatable, ChannelKeyWrapper, Protocol};
 
 use std::convert::TryFrom;
 use std::convert::TryInto;
 use std::fmt::Debug;
 
 type Bytes = Vec<u8>; // as in prost
+
+pub trait AccumulatorWrapper {
+    fn accumulate(&mut self, rhs: WriteToken);
+}
+
+impl<A> AccumulatorWrapper for A
+where
+    A: Into<Vec<Bytes>> + Accumulatable + From<WriteToken>,
+{
+    fn accumulate(&mut self, rhs: WriteToken) {
+        self.accumulate(rhs.into());
+    }
+}
 
 pub trait ProtocolWrapper {
     fn broadcast(&self, message: Bytes, key: ChannelKeyWrapper) -> Vec<WriteToken>;
@@ -14,13 +27,12 @@ pub trait ProtocolWrapper {
     fn gen_audit(&self, keys: Vec<ChannelKeyWrapper>, token: WriteToken) -> Vec<AuditShare>;
     fn check_audit(&self, tokens: Vec<AuditShare>) -> bool;
 
-    // fn new_accumulator(&self) -> Self::Accumulator {
-    //     Self::Accumulator::new(self.num_channels())
-    // }
-
-    // fn to_accumulator(&self, token: Self::WriteToken) -> Self::Accumulator;
+    fn new_accumulator(&self) -> Box<dyn AccumulatorWrapper>;
 }
 
+// some of these bounds are redundant:
+// - we know P::Accumulator: Into<Vec<P::Message>> and P::Message: Into<Bytes>, so we must be able to convert directly P::Accumulator: Into<Vec<Bytes>>
+// - similarly for P::Accumulator: From<P::WriteToken> and P::WriteToken: From<WriteToken>
 impl<P> ProtocolWrapper for P
 where
     P: Protocol,
@@ -31,6 +43,7 @@ where
     <P::WriteToken as TryFrom<WriteToken>>::Error: Debug,
     P::AuditShare: TryFrom<AuditShare> + Into<AuditShare>,
     <P::AuditShare as TryFrom<AuditShare>>::Error: Debug,
+    P::Accumulator: 'static + Into<Vec<Bytes>> + From<WriteToken>,
 {
     fn broadcast(&self, message: Bytes, key: ChannelKeyWrapper) -> Vec<WriteToken> {
         let message = message.into();
@@ -55,5 +68,9 @@ where
         let tokens: Result<Vec<P::AuditShare>, _> =
             tokens.into_iter().map(TryInto::try_into).collect();
         self.check_audit(tokens.unwrap())
+    }
+
+    fn new_accumulator(&self) -> Box<dyn AccumulatorWrapper> {
+        Box::new(self.new_accumulator())
     }
 }
