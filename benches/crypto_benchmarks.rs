5580243b66a483679b97f951ff3dba8a3f785992
Sacha Servan-Schreiber <sachaservanschreiber@gmail.com>
2020-03-29 22:18:28 -0400

    benchmarks for group and group-based prg

--- benches/crypto_benchmarks.rs
+++ benches/crypto_benchmarks.rs
@@ -8,23 +8,39 @@ use spectrum_impl::{
     crypto::{
         dpf::{DPF, PRGDPF},
         field::Field,
-        prg::{AESPRG, PRG},
+        group::Group,
+        prg::{GroupPRG, AESPRG, PRG},
         vdpf::{FieldVDPF, VDPF},
     },
 };
 
-const EVAL_SIZE: usize = 1 << 20; // approx 1MB
-
+const EVAL_SIZE: usize = 1 << 20; // (in bytes) approx 1MB
+const GENERATOR_SEED_BYTES: [u8; 16] = [
+    0x97, 0x32, 0x9c, 0x90, 0x21, 0xe6, 0x95, 0xfc, 0x1e, 0xdc, 0xa1, 0x32, 0x9c, 0x93, 0x2e, 0xb7,
+];
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("PRG eval benchmark", |b| {
+    c.bench_function("PRG eval", |b| {
         let prg = AESPRG::new(16, EVAL_SIZE);
         let seed = prg.new_seed();
         b.iter(|| prg.eval(&seed))
     });
 
+    c.bench_function("group PRG eval", |b| {
+        let factor: usize = EVAL_SIZE * 8 / 256; // expansion factor (# group elements each of 256 bits)
+        let prg = GroupPRG::new(factor, GENERATOR_SEED_BYTES);
+        let seed = prg.new_seed();
+        b.iter(|| prg.eval(&seed))
+    });
+
+    c.bench_function("group operation", |b| {
+        let el1 = Group::new_element(5.into());
+        let el2 = Group::new_element(5.into());
+        b.iter(|| el1.clone() ^ &el2)
+    });
+
     let num_points = 1;
     let point_idx = 0;
-    c.bench_function("PRGDPF eval benchmark", |b| {
+    c.bench_function("DPF (AES) eval", |b| {
         let dpf = PRGDPF::new(AESPRG::new(16, EVAL_SIZE), num_points);
         let data = Bytes::empty(EVAL_SIZE);
         let keys = dpf.gen(data, point_idx);
