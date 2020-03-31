980f35c08508b6481bed4602683d9619a4808dee
Sacha Servan-Schreiber <sachaservanschreiber@gmail.com>
2020-03-30 22:54:31 -0400

    style and testing

--- src/crypto/group.rs
+++ src/crypto/group.rs
@@ -27,16 +27,24 @@ pub struct GroupElement(ECFieldElement);
 
 impl Group {
     /// additive identity in the elliptic curve field
-    pub fn identity() -> GroupElement {
+    pub fn order_byte_size() -> usize {
+        32 // 32 bytes is the size of the group elements
+    }
+
+    /// additive identity in the elliptic curve field
+    pub fn additive_identity() -> GroupElement {
         GroupElement(ECFieldElement::zero())
     }
 
+    pub fn multiplicative_identity() -> GroupElement {
+        GroupElement(ECFieldElement::one())
+    }
     /// creates a new group element from an integer
-    pub fn new_element(value: Integer) -> GroupElement {
-        let val_mod = value % &Group::order();
-        let mut digits: [u8; 32] = [0x0u8; 32];
-        val_mod.write_digits(&mut digits, BYTE_ORDER);
-        GroupElement(ECFieldElement::from_bytes(&digits).unwrap())
+    pub fn new_element(value: &Integer) -> GroupElement {
+        let mut digits: [u8; 64] = [0x0u8; 64];
+        let value_reduced = value % Group::order();
+        value_reduced.write_digits(&mut digits, BYTE_ORDER);
+        GroupElement(ECFieldElement::from_bytes_wide(&digits))
     }
 
     /// new element from little endian bytes
@@ -47,7 +55,6 @@ impl Group {
     /// generates a new random group element
     pub fn rand_element() -> GroupElement {
         // generate enough random bytes to create a random element in the group
-        // size of group is obtained via modulus.significant_digits
         let mut rand_bytes: [u8; 64] = [0; 64];
         thread_rng().fill_bytes(&mut rand_bytes);
         Self::element_from_bytes(rand_bytes)
@@ -85,9 +92,9 @@ impl Group {
 }
 
 impl GroupElement {
-    pub fn exp(&self, pow: &Integer) -> GroupElement {
-        let pow_mod = Integer::from(pow % &Group::order());
-        GroupElement(self.0.mul(&Group::new_element(pow_mod).0))
+    pub fn pow(&self, pow: &Integer) -> GroupElement {
+        // in EC group law is addition, so exponentiation = multiplying
+        GroupElement(self.0 * Group::new_element(pow).0)
     }
 }
 
@@ -138,14 +145,15 @@ impl PartialEq for GroupElement {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::bytes::Bytes;
     use proptest::prelude::*;
     use rug::integer::IsPrime;
     use std::ops::Range;
-    const MAX_USIZE: usize = usize::max_value();
     const NUM_GROUP_GENERATORS: Range<usize> = 1..500;
 
     pub fn integers() -> impl Strategy<Value = Integer> {
-        (0..MAX_USIZE).prop_map(Integer::from)
+        any_with::<Bytes>(64.into())
+            .prop_map(|bytes| Integer::from_digits(&bytes.as_ref(), BYTE_ORDER))
     }
 
     impl Arbitrary for GroupElement {
@@ -154,7 +162,7 @@ mod tests {
 
         fn arbitrary_with(_: Self::Parameters) -> Self::Strategy {
             integers()
-                .prop_map(move |value| Group::new_element(value))
+                .prop_map(move |value| Group::new_element(&value))
                 .boxed()
         }
     }
@@ -165,9 +173,12 @@ mod tests {
 
     pub fn integer_triplet() -> impl Strategy<Value = (Integer, Integer, Integer)> {
         (
-            (0..MAX_USIZE).prop_map(Integer::from),
-            (0..MAX_USIZE).prop_map(Integer::from),
-            (0..MAX_USIZE).prop_map(Integer::from),
+            any_with::<Bytes>(64.into())
+                .prop_map(|bytes| Integer::from_digits(&bytes.as_ref(), BYTE_ORDER)),
+            any_with::<Bytes>(64.into())
+                .prop_map(|bytes| Integer::from_digits(&bytes.as_ref(), BYTE_ORDER)),
+            any_with::<Bytes>(64.into())
+                .prop_map(|bytes| Integer::from_digits(&bytes.as_ref(), BYTE_ORDER)),
         )
     }
 
@@ -199,53 +210,36 @@ mod tests {
         }
 
         #[test]
-        fn test_op_identity((x, y) in group_element_pairs()) {
-            assert_eq!(x.clone() ^ y.clone() ^ Group::identity(), x ^ y);
-        }
-
-        #[test]
-        fn test_exp_prod_in_exponent((a, b, c) in integer_triplet()) {
-            let el = Group::rand_element();
-            let prod = Integer::from(&a*&b) * c.clone();
-            assert_eq!(el.exp(&a).exp(&b).exp(&c), el.exp(&prod))
+        fn test_add_identity(element in any::<GroupElement>()) {
+            assert_eq!(element.clone() ^ Group::additive_identity(), Group::additive_identity() ^ element);
         }
 
 
         #[test]
-        fn test_op_mod_in_exponent(val in (0..MAX_USIZE).prop_map(Integer::from)) {
-            let el = Group::rand_element();
-            let modulo = Group::order();
-            let val_pow = val.pow_mod(&Integer::from(4), &(modulo.clone()*2)).unwrap();
-            let val_pow_reduced = Integer::from(&val_pow % &modulo);
-            assert_eq!(el.exp(&val_pow), el.exp(&val_pow_reduced))
-        }
-
-
-        #[test]
-        fn test_op_sums_in_exponent((a, b, c) in integer_triplet()) {
-            let el = Group::rand_element();
-            let modulo = Group::order();
-            let a_pow = a.pow_mod(&Integer::from(4), &modulo).unwrap();
-            let b_pow = b.pow_mod(&Integer::from(4), &modulo).unwrap();
-            let c_pow = c.pow_mod(&Integer::from(4), &modulo).unwrap();
-            let sum = Integer::from(&a_pow + &b_pow) + c_pow.clone();
-            assert_eq!(el.exp(&a_pow)^el.exp(&b_pow)^el.exp(&c_pow), el.exp(&sum))
+        fn test_exp_prod(el in any::<GroupElement>(), (a, b, c) in integer_triplet()) {
+            let prod = a.clone() * b.clone() * c.clone();
+            assert_eq!(el.pow(&a).pow(&b).pow(&c), el.pow(&prod))
         }
 
         #[test]
-        fn test_generators(num in NUM_GROUP_GENERATORS) {
-            let mut rand_seed: [u8; 16] = [0; 16];
-            thread_rng().fill_bytes(&mut rand_seed);
-            assert_eq!(Group::generators(num, &rand_seed), Group::generators(num, &rand_seed));
+        fn test_sums_in_exponent(el in any::<GroupElement>(), (a, b, c) in integer_triplet()) {
+            let sum = a.clone() + b.clone() + c.clone();
+            assert_eq!(el.pow(&a)^el.pow(&b)^el.pow(&c), el.pow(&sum))
         }
 
         #[test]
-        fn test_generators_different_seeds(num in NUM_GROUP_GENERATORS) {
-            let mut rand_seed_1: [u8; 16] = [0; 16];
-            let mut rand_seed_2: [u8; 16] = [0; 16];
-            thread_rng().fill_bytes(&mut rand_seed_1);
-            thread_rng().fill_bytes(&mut rand_seed_2);
-            assert_ne!(Group::generators(num, &rand_seed_1), Group::generators(num, &rand_seed_2));
+        fn test_generators_deterministic(
+            num in NUM_GROUP_GENERATORS,
+            seed in any::<[u8; 16]>()) {
+            assert_eq!(Group::generators(num, &seed), Group::generators(num, &seed));
+        }
+
+        #[test]
+        fn test_generators_different_seeds_different_generators(
+            num in NUM_GROUP_GENERATORS,
+            seed1 in any::<[u8; 16]>(),
+            seed2 in any::<[u8; 16]>()) {
+            assert_ne!(Group::generators(num, &seed1), Group::generators(num, &seed2));
         }
     }
 }
