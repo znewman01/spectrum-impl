4478aa6c9a1514d636ecb7c7e557533f63e18649
Sacha Servan-Schreiber <sachaservanschreiber@gmail.com>
2020-04-05 19:41:07 -0400

    style nits

--- src/crypto/group.rs
+++ src/crypto/group.rs
@@ -12,13 +12,20 @@ use std::ops;
 const BYTE_ORDER: Order = Order::LsfLe;
 
 // see jubjub::Fr for details
-// TODO: if we continue using JubJub, we should send them a PR to make this public.
+// PR to expose this as public within the library:
+// https://github.com/zkcrypto/jubjub/pull/34
 const JUBJUB_MODULUS: [u64; 4] = [
     0xd097_0e5e_d6f7_2cb7_u64,
     0xa668_2093_ccc8_1082_u64,
     0x0667_3b01_0134_3b00_u64,
     0x0e7d_b4ea_6533_afa9_u64,
 ];
+
+// size of group elements in jubjbu
+const JUBJUB_MODULUS_BYTES: usize = 32;
+
+// max number of bytes that can be cast to a jubjub group element
+const JUBJUB_MAX_CONVERT_BYTES: usize = 64;
 
 /// mathematical group object
 #[derive(Default, Clone, Eq, PartialEq, Debug)]
@@ -36,24 +43,19 @@ impl Group {
 
     /// creates a new group element from an integer
     pub fn new_element(value: &Integer) -> GroupElement {
-        assert!(
-            value.significant_digits::<u8>() <= 64,
-            "new element should be at most 64 bytes"
-        );
-
-        let mut digits: [u8; 64] = [0x0u8; 64];
-        value.write_digits(&mut digits, BYTE_ORDER);
+        // need to remove all extra significant bits to ensure that
+        // the value fits into a JUBJUB_MAX_CONVERT_BYTES byte array
+        // see significant_digits:
+        // https://docs.rs/rug/1.7.0/rug/struct.Integer.html#method.significant_digits
+        let cutoff_bits: u32 = 8 * JUBJUB_MAX_CONVERT_BYTES as u32;
+        let mut digits: [u8; JUBJUB_MAX_CONVERT_BYTES] = [0x0u8; JUBJUB_MAX_CONVERT_BYTES];
+        Integer::from(value.keep_bits_ref(cutoff_bits)).write_digits(&mut digits, BYTE_ORDER);
         GroupElement(ECFieldElement::from_bytes_wide(&digits))
     }
 
     /// new element from little endian bytes
     pub fn element_from_bytes(bytes: Bytes) -> GroupElement {
-        assert!(
-            bytes.len() <= 64,
-            "cannot cast more than 64 bytes into a group element"
-        );
-
-        let mut bytes_arr: [u8; 64] = [0; 64];
+        let mut bytes_arr: [u8; JUBJUB_MAX_CONVERT_BYTES] = [0; JUBJUB_MAX_CONVERT_BYTES];
         bytes_arr.copy_from_slice(bytes.as_ref());
         GroupElement(ECFieldElement::from_bytes_wide(&bytes_arr))
     }
@@ -61,7 +63,7 @@ impl Group {
     /// generates a new random group element
     pub fn rand_element() -> GroupElement {
         // generate enough random bytes to create a random element in the group
-        let mut bytes = vec![0; 32];
+        let mut bytes = vec![0; JUBJUB_MAX_CONVERT_BYTES];
         thread_rng().fill_bytes(&mut bytes);
         Self::element_from_bytes(bytes.into())
     }
@@ -70,13 +72,15 @@ impl Group {
     /// which are generators for the group (given that the group is of prime order)
     /// takes as input a random seed which deterministically generates [num] field elements
     pub fn generators(num: usize, seed: &AESSeed) -> Vec<GroupElement> {
-        let prg = AESPRG::new(16, 64 * num);
+        let prg = AESPRG::new(16, JUBJUB_MAX_CONVERT_BYTES * num);
         let rand_bytes: Vec<u8> = prg.eval(seed).into();
 
         //TODO: maybe use itertools::Itertools chunks?
         (0..num)
             .map(|i| {
-                let chunk = rand_bytes[i * 64..(i + 1) * 64].to_vec();
+                let chunk = rand_bytes
+                    [i * JUBJUB_MAX_CONVERT_BYTES..(i + 1) * JUBJUB_MAX_CONVERT_BYTES]
+                    .to_vec();
                 Group::element_from_bytes(Bytes::from(chunk))
             })
             .collect()
@@ -88,13 +92,13 @@ impl Group {
     }
 
     pub fn order_size_in_bytes() -> usize {
-        32 // 32 bytes is the size of the group elements
+        JUBJUB_MODULUS_BYTES // size of the group elements
     }
 }
 
 impl GroupElement {
     pub fn pow(&self, pow: &Integer) -> GroupElement {
-        // in EC group opertion is addition, so exponentiation = multiplying
+        // in EC group operation is addition, so exponentiation = multiplying
         GroupElement(self.0 * Group::new_element(pow).0)
     }
 }
@@ -156,7 +160,7 @@ mod tests {
     // need to generate 512-bit integers to ensure all operations
     // "wrap around" the group order during testing
     fn integer_512_bits() -> impl Strategy<Value = Integer> {
-        any_with::<Bytes>(63.into())
+        any_with::<Bytes>(JUBJUB_MAX_CONVERT_BYTES.into())
             .prop_map(|bytes| Integer::from_digits(&bytes.as_ref(), BYTE_ORDER))
     }
 
@@ -185,21 +189,24 @@ mod tests {
 
         #[test]
         fn test_add_identity(element: GroupElement) {
-            assert_eq!(element.clone() * Group::identity(), Group::identity() * element);
+            assert_eq!(element.clone() * Group::identity(), Group::identity() * element.clone());
+            assert_eq!(element.clone() * Group::identity(), element.clone());
+            assert_eq!(element.clone(), Group::identity() * element);
+
         }
 
 
         #[test]
         fn test_exp_prod(element: GroupElement, a in integer_512_bits(), b in integer_512_bits()) {
             let prod = a.clone() * b.clone();
-            let prod_reduced = prod % Group::order();
-            assert_eq!(element.pow(&a).pow(&b), element.pow(&prod_reduced))
+            let expected = prod % Group::order();
+            assert_eq!(element.pow(&a).pow(&b), element.pow(&expected))
         }
 
         #[test]
         fn test_sums_in_exponent(element: GroupElement, a in integer_512_bits(), b in integer_512_bits()) {
-            let sum = a.clone() + b.clone();
-            assert_eq!(element.pow(&a) * element.pow(&b), element.pow(&sum))
+            let expected = a.clone() + b.clone() % Group::order();
+            assert_eq!(element.pow(&a) * element.pow(&b), element.pow(&expected))
         }
 
         #[test]
@@ -213,7 +220,8 @@ mod tests {
         fn test_generators_different_seeds_different_generators(
             num in NUM_GROUP_GENERATORS,
             seed1: AESSeed,
-            seed2: AESSeed) {
+            seed2: AESSeed
+        ) {
             prop_assume!(seed1 != seed2, "Different generators only for different seeds");
             assert_ne!(Group::generators(num, &seed1), Group::generators(num, &seed2));
         }
