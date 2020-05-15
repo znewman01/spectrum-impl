ae145301c3719cdd7be499a708261b4d8cd11e21
Zachary Newman <z@znewman.net>
2020-05-15 09:36:00 -0400

    disable tc

--- experiments/src/infrastructure.rs
+++ experiments/src/infrastructure.rs
@@ -249,7 +249,7 @@ pub fn setup<H: std::hash::BuildHasher>(
                 .instance_type(machine_type)
                 .setup(move |ssh, log| {
                     install_spectrum(log, ssh, &s3_binary, workers_per_machine)?;
-                    ssh.cmd("sudo tc qdisc add dev ens5 root netem delay 20ms")?;
+                    // ssh.cmd("sudo tc qdisc add dev ens5 root netem delay 20ms")?;
                     Ok(())
                 }),
         )?;
@@ -269,7 +269,7 @@ pub fn setup<H: std::hash::BuildHasher>(
                 .instance_type(machine_type)
                 .setup(move |ssh, log| {
                     install_spectrum(log, ssh, &s3_binary, workers_per_machine)?;
-                    ssh.cmd("sudo tc qdisc add dev ens5 root netem delay 100ms")?;
+                    // ssh.cmd("sudo tc qdisc add dev ens5 root netem delay 100ms")?;
                     Ok(())
                 }),
         )?;
