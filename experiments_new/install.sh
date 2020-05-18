0c760f6c86b879d539421d828078326771aca96d
Zachary Newman <z@znewman.net>
2020-05-18 09:34:14 -0400

    Start installing/configuring everything

--- experiments_new/install.sh
+++ experiments_new/install.sh
@@ -1,13 +1,16 @@
-#!/bin/bash -e
+#!/bin/bash
 set -x
+set -eufo pipefail
 
-sudo apt update -y
-sudo apt install -y \
+sudo apt-get update -y
+sudo apt-get install -y \
      build-essential \
      libssl-dev \
      pkg-config \
      unzip \
-     m4
+     m4 \
+     etcd \
+     nginx
 
 curl https://sh.rustup.rs -sSf | sh -s -- \
     -y \
@@ -15,5 +18,25 @@ curl https://sh.rustup.rs -sSf | sh -s -- \
 
 curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip \
     -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip -q awscliv2.zip
 sudo ./aws/install
+
+sudo cp "$HOME/config/nginx.conf" /etc/nginx/nginx.conf
+sudo cp "$HOME/config/sysctl.conf" /etc/sysctl.d/20-spectrum.conf
+
+# Nginx pass-throughs
+# This will probably not work when we want to run on multiple servers
+externals=(6000 5001 5100 5101 5102 5103)
+internals=(6000 6001 6100 6101 6102 6103)
+for (( i=0; i<${#internals[*]}; ++i)); do
+    export internal=${internals[$i]}
+    export external=${externals[$i]}
+    envsubst '$internal $external' < "${HOME}/config/nginx.conf.template" \
+        | sudo tee "/etc/nginx/conf.d/proxy-${external}-${internal}.conf" > /dev/null
+done
+
+sudo mv "${HOME}/config/publisher.service" "/etc/systemd/system/spectrum-publisher.service"
+sudo mv "${HOME}/config/leader.service" "/etc/systemd/system/spectrum-leader.service"
+sudo mv "${HOME}/config/worker@.service" "/etc/systemd/system/spectrum-worker@.service"
+sudo mv "${HOME}/config/viewer@.service" "/etc/systemd/system/viewer@.service"
+sudo mv "${HOME}/config/broadcaster@.service" "/etc/systemd/system/broadcaster@.service"
