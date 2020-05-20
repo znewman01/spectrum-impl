0ba826aa46e071c3518e1e5eaa05fdd19292d58c
Zachary Newman <z@znewman.net>
2020-05-20 13:39:25 -0400

    Add SSH convenience script

---
+++ experiments_new/ssh.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import argparse
+import sys
+import json
+import os
+import tempfile
+
+from subprocess import check_output, check_call
+
+def main():
+    parser = argparse.ArgumentParser(description="SSH into a Terraform machine.")
+    parser.add_argument("--publisher", action="store_true", help="SSH into the publisher machine (default)")
+    parser.add_argument("--client", type=int, nargs="?", default=0, help="SSH into some/(the nth) client machine")
+    parser.add_argument("--worker", type=int, nargs="?", default=0, help="SSH into some/(the nth) worker machine")
+    args = parser.parse_args()
+
+    data = json.loads(check_output(["terraform", "output", "-json"]))
+    data = {k: v["value"] for k, v in data.items()}
+
+    if args.client is not None:
+        hostname = data["clients"][args.client]
+    elif args.worker is not None:
+        hostname = data["workers"][args.worker]
+    else:
+        hostname = data["publisher"]
+
+    with tempfile.NamedTemporaryFile() as f:
+        f.write(data["private_key"].encode("utf8"))
+        f.flush()
+
+        check_call(["ssh", "-i", f.name, f"ubuntu@{hostname}", "-o", "StrictHostKeyChecking=no"])
+
+if __name__ == '__main__':
+    main()
