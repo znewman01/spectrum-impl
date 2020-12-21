71b093d78aa5e4c1593d63ffe2cf9f42ec2955c2
Zachary Newman <z@znewman.net>
2020-12-21 10:17:52 -0500

    Add express/riposte to experiment generation

--- data/make_experiments.py
+++ data/make_experiments.py
@@ -6,25 +6,21 @@ import sys
 
 TRIALS = 5
 
+MESSAGE_SIZES = [1_000, 10_000, 50_000, 100_000, 500_000, 1_000_000]
+CHANNELS = [100, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000]
+
 # Doesn't include "full broadcast" plots which are a special case.
 PLOTS_SPECTRUM = {
     # "Our turf": one channel, many clients, measure QPS
-    "onechannel": {
-        "channels": [1],
-        "clients": [1000],
-        "message_size": [1_000, 10_000, 50_000, 100_000, 500_000, 1_000_000],
-    },
+    "onechannel": {"channels": [1], "clients": [1000], "message_size": MESSAGE_SIZES,},
     # "Their turf": number clients = number channels
-    "manychannel": {
-        "clients": [100, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000],
-        "message_size": [1_000, 10_000, 100_000, 1_000_000],
-    },
+    "manychannel": {"channels": CHANNELS, "message_size": MESSAGE_SIZES,},
     # Horizontal scaling experiment
     "horizontal": {
         "worker_machines_per_group": [1, 2, 3, 4, 5, 6, 7, 8],
         # too many total workers appears to lead to higher tail latencies
         "workers_per_machine": [4],
-        "message_size": [125_000],
+        "message_size": [100_000],
         "clients": [1500],
         "channels": [500],
     },
@@ -33,7 +29,7 @@ PLOTS_SPECTRUM = {
         "clients": [1400],
         "clients_per_machine": [200],
         "channels": [100],
-        "message_size": [125_000],
+        "message_size": [100_000],
         "protocol": [
             {"SeedHomomorphic": {"parties": 2,},},
             {"SeedHomomorphic": {"parties": 3,},},
@@ -43,13 +39,22 @@ PLOTS_SPECTRUM = {
     "multi-server-control": {
         "clients": [1400],
         "channels": [100],
-        "message_size": [125_000],
+        "message_size": [100_000],
         "protocol": [{"Symmetric": {"security": 16,}}],
     },
 }
 
+PLOTS_EXPRESS = {
+    "onechannel": {"channels": [1], "message_size": MESSAGE_SIZES,},
+    "manychannel": {"channels": CHANNELS, "message_size": MESSAGE_SIZES,},
+}
 
-def _gen_experiments(trials, params):
+PLOTS_RIPOSTE = {
+    "manychannel": {"channels": CHANNELS, "message_size": MESSAGE_SIZES,},
+}
+
+
+def make_experiments(trials, params):
     # params is dict of key:list
     keys = list(params)
     for values in itertools.product(*[params[k] for k in keys]):
@@ -57,8 +62,10 @@ def _gen_experiments(trials, params):
             yield dict(zip(keys, values))
 
 
-def make_experiments(trials, params):
-    for experiment in _gen_experiments(trials, params):
+def make_experiments_spectrum(trials, params):
+    # support for "full broadcast" plots (# channels = # clients) by omitting clients/channels
+    # other services don't need this
+    for experiment in make_experiments(trials, params):
         if "clients" in experiment and "channels" not in experiment:
             experiment["channels"] = experiment["clients"]
         elif "channels" in experiment and "clients" not in experiment:
@@ -66,10 +73,9 @@ def make_experiments(trials, params):
         yield experiment
 
 
-def _write_file(path, params):
+def _write_file(path, data):
     with open(path, "w") as f:
-        experiments = list(make_experiments(TRIALS, params))
-        json.dump(experiments, f, indent=2)
+        json.dump(data, f, indent=2)
 
 
 def main(args):
@@ -79,7 +85,18 @@ def main(args):
 
     for name, params in PLOTS_SPECTRUM.items():
         path = os.path.join(args.output_dir, f"experiments-spectrum-{name}.json")
-        _write_file(path, params)
+        experiments = list(make_experiments_spectrum(TRIALS, params))
+        _write_file(path, experiments)
+
+    for name, params in PLOTS_EXPRESS.items():
+        path = os.path.join(args.output_dir, f"experiments-express-{name}.json")
+        experiments = list(make_experiments(TRIALS, params))
+        _write_file(path, experiments)
+
+    for name, params in PLOTS_EXPRESS.items():
+        path = os.path.join(args.output_dir, f"experiments-riposte-{name}.json")
+        experiments = list(make_experiments(TRIALS, params))
+        _write_file(path, experiments)
 
 
 if __name__ == "__main__":
