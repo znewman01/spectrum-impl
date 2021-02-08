50f290d0c283d75bf0c09af26adcb5b21cdebeda
Zachary Newman <z@znewman.net>
2021-02-08 13:33:03 -0500

    Add more metadata to experiment results folder

--- data/run_experiments.sh
+++ data/run_experiments.sh
@@ -45,12 +45,26 @@ main() {
     var="ran_${system}"
     if [ ! -z ${!var} ]; then
       if [ $system = "spectrum" ]; then
+        # Local stuff: date, Rust version, LoC count
+        date "+%B %Y" \
+          > ${TMP_DIR}/results/experiment-date.txt
+        rustc --version | grep -Eo '20[0-9]{2}-[0-9]{2}-[0-9]{2}' \
+          > ${TMP_DIR}/results/rust-nightly-date.txt
+        tokei --output json > ${TMP_DIR}/results/loc.json
+        # AWS instance information
         python -m experiments.spectrum.ssh --worker -- \
-          "ec2metadata | grep instance-type" \
+          "ec2metadata | grep instance-type | sed 's/instance-type: //'" \
           > ${TMP_DIR}/results/instance-type.txt
+        instance_type=$(cat ${TMP_DIR}/results/instance-type.txt  | tr -d '\n')
+        aws ec2 --region us-east-2 \
+          describe-instance-types --instance-types=${instance_type} \
+          > ${TMP_DIR}/results/instance.json
+        curl -sL "https://ec2.shop?filter=${instance_type}" -H 'accept: json' \
+          > ${TMP_DIR}/results/instance-cost.json
+        # Performance
         python -m experiments.spectrum.ssh --worker -- \
           "openssl speed -elapsed -evp aes-128-ctr 2>&1" \
-          > ${TMP_DIR}/results/openssl-speed.txt
+          > ${TMP_DIR}/results/openssl-stderr.txt
       fi
       echo "[]" | python -m experiments --cleanup ${system} -
     fi
