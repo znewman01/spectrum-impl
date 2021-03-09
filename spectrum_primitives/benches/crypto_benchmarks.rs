857be123e3bb54f789b17437bf9101ce7f7ea2f1
Zachary Newman <z@znewman.net>
2021-03-08 20:37:29 -0500

    squelch a final warning

--- spectrum_primitives/benches/crypto_benchmarks.rs
+++ spectrum_primitives/benches/crypto_benchmarks.rs
@@ -1,3 +1,4 @@
+#![allow(clippy::identity_op)]
 use criterion::{criterion_group, criterion_main, BatchSize, BenchmarkId, Criterion, Throughput};
 use rand::thread_rng;
 use spectrum_primitives::{Bytes, Dpf, MultiKeyVdpf, TwoKeyVdpf, Vdpf};
@@ -12,6 +13,18 @@ fn criterion_benchmark(c: &mut Criterion) {
         group.throughput(Throughput::Bytes(*size as u64));
         group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
             let dpf = TwoKeyVdpf::with_channels_msg_size(1, size);
+            let keys = dpf.gen_empty();
+            let key = &keys[0];
+            b.iter_batched(|| key.clone(), |key| dpf.eval(key), BatchSize::LargeInput)
+        });
+    }
+    group.finish();
+
+    let mut group = c.benchmark_group("DPF (SH) Evaluation");
+    for size in SIZES.iter() {
+        group.throughput(Throughput::Bytes(*size as u64));
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+            let dpf = MultiKeyVdpf::with_channels_parties_msg_size(1, 3, size);
             let keys = dpf.gen_empty();
             let key = &keys[0];
             b.iter_batched(|| key.clone(), |key| dpf.eval(key), BatchSize::LargeInput)
@@ -53,6 +66,25 @@ fn criterion_benchmark(c: &mut Criterion) {
     for size in SIZES.iter() {
         group.throughput(Throughput::Bytes(*size as u64));
         group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+            let vdpf = TwoKeyVdpf::with_channels_msg_size(1, size);
+            let auth_keys = vdpf.new_access_keys();
+            let dpf_keys = vdpf.gen_empty();
+            let proof_shares = vdpf.gen_proofs_noop();
+            let dpf_key = &dpf_keys[0];
+            let proof_share = &proof_shares[0];
+            b.iter_batched(
+                || proof_share.clone(),
+                |proof| vdpf.gen_audit(&auth_keys, dpf_key, proof),
+                BatchSize::LargeInput,
+            )
+        });
+    }
+
+    group.finish();
+    let mut group = c.benchmark_group("Vdpf.gen_audit() (SH)");
+    for size in SIZES.iter() {
+        group.throughput(Throughput::Bytes(*size as u64));
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
             let vdpf = MultiKeyVdpf::with_channels_parties_msg_size(1, 3, size);
             let auth_keys = vdpf.new_access_keys();
             let dpf_keys = vdpf.gen_empty();
@@ -66,6 +98,7 @@ fn criterion_benchmark(c: &mut Criterion) {
             )
         });
     }
+    group.finish();
 
     // TODO: more benchmarks
     // - GroupPRG
