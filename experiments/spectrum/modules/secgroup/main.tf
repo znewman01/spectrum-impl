2e8f40b38ccda7fcc67bb56c595c5a1cedf596ed
Zachary Newman <z@znewman.net>
2021-09-02 12:34:43 -0400

    fixes for manyserver spectrum security groups

--- experiments/spectrum/modules/secgroup/main.tf
+++ experiments/spectrum/modules/secgroup/main.tf
@@ -20,27 +20,18 @@ resource "aws_security_group_rule" "etcd" {
   description       = "Spectrum traffic internal: leader+publisher"
   from_port         = 2379
   to_port           = 2379
-  cidr_blocks       = [for i in var.instances : "${i.public_ip}/32"]
+  cidr_blocks       = [for i in var.instances : "${i.public_ip}/32" if i.public_ip != ""]
   protocol          = "tcp"
   security_group_id = var.security_group.id
 }
 
-resource "aws_security_group_rule" "leader_and_publisher" {
+# Should be 6000-60001 and 6100-6110, but we're bumping up against quotas.
+resource "aws_security_group_rule" "leader_and_publisher_and_workers" {
   type              = "ingress"
   description       = "Spectrum traffic internal: leader+publisher"
   from_port         = 6000
-  to_port           = 6001
-  cidr_blocks       = [for i in var.instances : "${i.public_ip}/32"]
-  protocol          = "tcp"
-  security_group_id = var.security_group.id
-}
-
-resource "aws_security_group_rule" "workers" {
-  type              = "ingress"
-  description       = "Spectrum traffic internal: leader+publisher"
-  from_port         = 6100
   to_port           = 6110
-  cidr_blocks       = [for i in var.instances : "${i.public_ip}/32"]
+  cidr_blocks       = [for i in var.instances : "${i.public_ip}/32" if i.public_ip != ""]
   protocol          = "tcp"
   security_group_id = var.security_group.id
 }
